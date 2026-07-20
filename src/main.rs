9fb10c7bd7ff83e0c536a8e8c95f567a98d9816a
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2026-07-20 10:31:58 +0530

    createing multiple route but on single method

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,13 @@ use axum::{Router, routing::get};
 
 #[tokio::main]
 async fn main(){
-    let app = Router::new().route("/hello", get(|| async {"hello world"}));
+    async fn index()->&'static str{"home"}
+    async fn about()->&'static str{"about"}
+    async fn hello()->&'static str{"hello world"}
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/about", get(about))
+        .route("/hello", get(hello));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
     println!("Listing in http://127.0.0.1:3000");
