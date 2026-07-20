9880bbc2564bbce83471c2c1ff888317799ff9e1
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2026-07-20 10:26:12 +0530

    hello world in axum tokio rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main(){
+    let app = Router::new().route("/hello", get(|| async {"hello world"}));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    println!("Listing in http://127.0.0.1:3000");
+    axum::serve(listener, app).await.unwrap();
 }
